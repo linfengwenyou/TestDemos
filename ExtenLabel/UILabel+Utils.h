9c14b33c1504fcab95bdcb3a8336163a9ebfd5b6
fumi <fumi@fumideMacBook-Pro.local>
2018-08-30 09:56:01 +0800

    label添加尺寸限制

---
+++ ExtenLabel/UILabel+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Utils.h
+//  DemoDemo
+//
+//  Created by fumi on 2018/8/30.
+//  Copyright © 2018年 fumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Utils)
+/** 行数 */
+@property (nonatomic, assign, readonly) NSInteger lineNum;
+
+@end
