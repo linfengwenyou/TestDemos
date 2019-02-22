47a875252eec8de361bddcd1c797cf4b17c5bc29
fumi <fumi@fumideMacBook-Pro.local>
2019-02-22 13:53:57 +0800

    图片上传添加

---
+++ 待补充完成任务.playground/Pages/能效开发.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+/*:
+ 参考地址：
+ https://developer.apple.com/library/archive/documentation/Performance/Conceptual/EnergyGuide-iOS/MinimizeTimerUse.html#//apple_ref/doc/uid/TP40015243-CH41-SW1
+ */
+import Foundation
+
+
+//: [Next](@next)
